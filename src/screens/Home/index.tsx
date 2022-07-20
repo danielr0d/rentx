d2710720afa9c7b7efb11355a3c9c7c598782234
danielr0d <daniel.rodrigues.98@hotmail.com>
2022-07-20 16:39:40 -0300

    add car component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,22 @@ import { Container, Header, HeaderContent, TotalCars } from './styles'
 
 import { RFValue } from 'react-native-responsive-fontsize'
 
+import { Car } from '../../components/Car'
+
 import Logo from '../../assets/logo.svg'
 import { StatusBar } from 'expo-status-bar'
 
 export function Home() {
+  const carData = {
+    brand: 'VW',
+    name: 'Fuscao',
+    rent: {
+      period: 'AO DIA',
+      price: 120
+    },
+    thumbnail: 'https://autopecasabj.com.br/image/cache/data/CARROS/fusca-500x500.png'
+  }
+
   return (
     <Container>
       <StatusBar backgroundColor="transparent" translucent />
@@ -16,6 +28,8 @@ export function Home() {
           <TotalCars>Total: 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
     </Container>
   )
 }
